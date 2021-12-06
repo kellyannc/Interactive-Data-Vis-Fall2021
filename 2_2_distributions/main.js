3b4c58be5e58032a6487a68a5efb75abb677eca5
kellyannc <kelly.cunningham@cuny.edu>
2021-12-05 22:43:36 -0500

    120521

--- 2_2_distributions/main.js
+++ 2_2_distributions/main.js
@@ -1,8 +1,9 @@
+//HomeWork _ using new dataset file
 /* CONSTANTS AND GLOBALS */
 const width = 800
   height = 600,
-  margin = 60,
-  radius =5;
+  margin = 12,
+  radius =10;
 
 /* LOAD DATA */
 d3.csv('../data/happiness.csv', d3.autoType)
@@ -14,27 +15,31 @@ d3.csv('../data/happiness.csv', d3.autoType)
       .attr("width", width)
       .attr("height", height)
       .style("background-color", "pink")
-      .style('border', '10px white') 
-      .style('font-size', '100px')        
-      .style('color', 'red')            
-      .style('text-align', 'right')    
-      .style('left-padding', '5px') 
+      // .style('border', '10px white') 
+      // .style('font-size', '100px')        
+      // .style('color', 'red')            
+      // .style('text-align', 'right')    
+      // .style('left-padding', '5px') 
 
     /* SCALES */
 
     const xScale = d3.scaleLinear()
     .domain(d3.extent(research, d => d.Country))
     .range([margin, width - margin])
+    // .nice()
   
     const yScale = d3.scaleLinear()
     .domain(d3.extent(research, d => d.Mean))
     .range([height - margin, margin])
+    // .nice()
 
-    // console.log('yScale.domain():>>', yScale.domain());
+    // // console.log('Scale.domain():>>', yScale.domain());
+    // console.log('xScale.domain():>>', xScale.domain());
+    // // console.log('yScale.domain():>>', yScale.domain());
 
     const colorscale = d3.scaleOrdinal()
-    .domain(["Male", "Female"])
-    .range(["yellow", "Purple"])
+    .domain(["Male", "Female", "Both"])
+    .range(["yellow", "Purple", "White"])
     
     /* HTML ELEMENTS */
 
@@ -49,7 +54,7 @@ d3.csv('../data/happiness.csv', d3.autoType)
 
     svg.append("g")
       .attr("class", "x-axis")
-      .style("transform", '0px,${margin - height}px)')
+      .style("transform", 'translate(0px,${height - margin}px)')
       .call(d3.axisBottom(xScale))
 
       svg.append("g")
@@ -59,7 +64,7 @@ d3.csv('../data/happiness.csv', d3.autoType)
   });
 
 
-//   tutorial
+//   tutorial II
 
 //   /* CONSTANTS AND GLOBALS */
 // const width = 600,
@@ -100,12 +105,23 @@ d3.csv('../data/happiness.csv', d3.autoType)
 
 //   svg.selectAll(".dot")
 //   .data(scores)
-//   .join("circle")
+//   .join(enter => enter
+//     .append("circle")
+//     .style("fill", "black")
+//     .attr("r", 0)
+//     .call(enter => enter.transition()
+//     .delay((d, i) => xScale(d.ideologyScore2020 * 50)) //dots moves left to right on screen 
+//     // .delay((d, i) => i * 500)
+//     .duration(500)
+//     .attr("r", radius)
+//     .style("fill", d => colorscale(d.Party))
+//     )
+//   )
+
 //   .attr("class", "dot")
 //   .attr("cx", d => xScale(d.ideologyScore2020))
 //   .attr("cy", d => yScale(d.envScore2020))
-//   .attr("r", radius)
-//   .style("fill", d => colorscale(d.Party))
+ 
 
 //   svg.append("g")
 //     .attr("class", "x-axis")
@@ -118,3 +134,64 @@ d3.csv('../data/happiness.csv', d3.autoType)
 //     .call(d3.axisRight(yScale))
 
 // });
+
+// Tutorial 1
+
+
+//   /* CONSTANTS AND GLOBALS */
+//   const width = 600,
+//   height = 600,
+//   margin = 40,
+//   radius =5;
+  
+//   /* LOAD DATA */
+//   d3.json("../data/environmentRatings.json", d3.autoType)
+//   .then(scores => {
+//     console.log(scores)
+  
+//     const svg = d3.select("#container")
+//       .append("svg")
+//       .attr("width", width)
+//       .attr("height", height)
+//       .style("background-color", "pink")
+  
+//     /* SCALES */
+  
+//     const xScale = d3.scaleLinear()
+//     .domain(d3.extent(scores, d => d.ideologyScore2020))
+//     .range([margin, width - margin])
+//     .nice()
+  
+//     const yScale = d3.scaleLinear()
+//     .domain(d3.extent(scores, d => d.envScore2020))
+//     .range([height - margin, margin])
+//     .nice()
+  
+//     // console.log('yScale.domain():>>', yScale.domain());
+  
+//     const colorscale = d3.scaleOrdinal()
+//     .domain(["R", "D", "I"])
+//     .range(["red", "blue", "purple"])
+    
+//     /* HTML ELEMENTS */
+  
+//     svg.selectAll(".dot")
+//     .data(scores)
+//     .join("circle")
+//     .attr("class", "dot")
+//     .attr("cx", d => xScale(d.ideologyScore2020))
+//     .attr("cy", d => yScale(d.envScore2020))
+//     .attr("r", radius)
+//     .style("fill", d => colorscale(d.Party))
+  
+//     svg.append("g")
+//       .attr("class", "x-axis")
+//       .style("transform", 'translate(0px,${margin - height}px)')
+//       .call(d3.axisBottom(xScale))
+  
+//       svg.append("g")
+//       .attr("class", "y-axis")
+//       .style("transform", 'translate(0px,0px)')
+//       .call(d3.axisRight(yScale))
+  
+//   });
